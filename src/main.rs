3901b537152ef7c9c8c3daa8ae6798e57bc21e4b
Dmitry Panin <pank.dm@gmail.com>
2020-07-08 23:13:25 -0700

    Full support for all 6 messages

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
+// Usage:
+// cargo run input_file output_file
+
+use std::env;
 use std::fs::File;
 use std::io::Write;
 
 use image::Rgb;
 
-const FILE_NAME: &str = "input/message2.png";
+// const FILE_NAME: &str = "input/message2.png";
 
 struct Svg {
     file: File,
@@ -12,10 +16,27 @@ struct Svg {
 const ZOOM: usize = 8;
 const SHIFT: usize = 2;
 
+fn annotation_text(control_bit: bool, value: i32) -> String {
+    if !control_bit {
+        return value.to_string();
+    }
+    let text = match value {
+        12 => "==",
+        417 => "inc",
+        401 => "dec",
+        _ => "",
+    };
+    return if text == "" {
+        value.to_string()
+    } else {
+        text.to_string()
+    };
+}
+
 #[allow(unused)]
 impl Svg {
-    pub fn new(width: usize, height: usize) -> Svg {
-        let mut file = File::create("output.svg").unwrap();
+    pub fn new(output_file: &String, width: usize, height: usize) -> Svg {
+        let mut file = File::create(output_file).unwrap();
         file.write_all(
             format!(
                 "<svg xmlns='http://www.w3.org/2000/svg' version='1.1' width='{}' height='{}'>\n",
@@ -44,14 +65,24 @@ impl Svg {
         );
     }
 
-    pub fn add_annotation(&mut self, x: usize, y: usize, dx: usize, dy: usize, value: i32) {
+    pub fn add_annotation(
+        &mut self,
+        x: usize,
+        y: usize,
+        dx: usize,
+        dy: usize,
+        value: i32,
+        control_bit: bool,
+    ) {
+        let color = if control_bit { "yellow" } else { "green" };
         self.file.write_all(
             format!(
-                "<rect x='{}' y='{}' width='{}' height='{}' style='fill:green;opacity:0.5'/>\n",
+                "<rect x='{}' y='{}' width='{}' height='{}' style='fill:{};opacity:0.5'/>\n",
                 x * ZOOM - SHIFT,
                 y * ZOOM - SHIFT,
                 dx * ZOOM + 2 * SHIFT,
                 dy * ZOOM + 2 * SHIFT,
+                color,
             )
             .as_bytes(),
         );
@@ -64,6 +95,7 @@ impl Svg {
             "font: 20px bold sans;",
         ]
         .join(" ");
+
         self.file.write_all(
             format!(
                 "<text x='{}' y='{}' {} style='{}'>{}</text>\n",
@@ -71,7 +103,7 @@ impl Svg {
                 y * ZOOM + (dy / 2) * ZOOM,
                 options,
                 style_options,
-                value,
+                annotation_text(control_bit, value),
             )
             .as_bytes(),
         );
@@ -110,17 +142,21 @@ struct ImageWrapper {
 
 enum ParseResult {
     None,
-    Integer { dx: usize, dy: usize, value: i32 },
+    Glyph {
+        dx: usize,
+        dy: usize,
+        value: i32,
+        control_bit: bool,
+    },
 }
 
 fn try_parse_symbol(iw: &ImageWrapper, x: usize, y: usize) -> ParseResult {
     let image = &iw.image;
-    if image[x][y] == 0
-        && image[x + 1][y] == 1
-        && image[x][y + 1] == 1
-        && image[x - 1][y] == 0
-        && image[x][y - 1] == 0
+    if image[x + 1][y] == 1 && image[x][y + 1] == 1 && image[x - 1][y] == 0 && image[x][y - 1] == 0
     {
+        // control bit
+        let control_bit = image[x][y] == 1;
+
         // Find proper delta
         let mut delta = 1;
         // 10 as limit should be good enough
@@ -143,14 +179,28 @@ fn try_parse_symbol(iw: &ImageWrapper, x: usize, y: usize) -> ParseResult {
                 }
             }
         }
+        let extra_bit = image[x][y + delta];
+        // extra bit indicate negative numbers
+        if extra_bit == 1 {
+            value = -value;
+        }
 
-        return ParseResult::Integer {
+        return ParseResult::Glyph {
             dx: delta,
-            dy: delta,
+            dy: delta + extra_bit as usize,
             value: value,
+            control_bit: control_bit,
         };
     } else {
         return ParseResult::None;
+    }
+}
+
+fn mark_parsed(parsed: &mut BooleanGrid, x: usize, y: usize, dx: usize, dy: usize) {
+    for cdx in 0..dx {
+        for cdy in 0..dy {
+            parsed[x + cdx][y + cdy] = true;
+        }
     }
 }
 
@@ -165,12 +215,19 @@ fn parse_image(iw: &ImageWrapper, parsed: &mut BooleanGrid, svg: &mut Svg) {
             let parse_result = try_parse_symbol(iw, x, y);
             match parse_result {
                 ParseResult::None => continue,
-                ParseResult::Integer { dx, dy, value } => {
+                ParseResult::Glyph {
+                    dx,
+                    dy,
+                    value,
+                    control_bit,
+                } => {
+                    let name = if control_bit { "Control" } else { "Integer" };
                     println!(
-                        "Found Integer at ({}, {}), value = {}, d = ({}, {})",
-                        x, y, value, dx, dy
+                        "Found {} Glyph at ({}, {}), value = {}, d = ({}, {})",
+                        name, x, y, value, dx, dy
                     );
-                    svg.add_annotation(x, y, dx, dy, value);
+                    mark_parsed(parsed, x, y, dx, dy);
+                    svg.add_annotation(x, y, dx, dy, value, control_bit);
                 }
             }
         }
@@ -178,13 +235,19 @@ fn parse_image(iw: &ImageWrapper, parsed: &mut BooleanGrid, svg: &mut Svg) {
 }
 
 fn main() {
-    let img = image::open(FILE_NAME).unwrap().to_rgb();
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
+    assert!(args.len() >= 2);
+    let input_file = args[1].to_string();
+    let output_file = args[2].to_string();
+
+    let img = image::open(input_file).unwrap().to_rgb();
     println!("Img dimensions: {:?}", img.dimensions());
     let scale = 4;
     let width = img.dimensions().0 / scale;
     let height = img.dimensions().1 / scale;
 
-    let mut svg = Svg::new(width as usize, height as usize);
+    let mut svg = Svg::new(&output_file, width as usize, height as usize);
 
     // initialize initial data structures
     let mut parsed = Vec::new();
